d210761d247034d6ddb8e044f5034c687202cba5
AnanyaK1 <ananyak6@illinois.edu>
2020-05-05 15:00:17 -0500

    Links provided

--- app/src/main/java/com/example/tictactoe/SecondScreen.java
+++ app/src/main/java/com/example/tictactoe/SecondScreen.java
@@ -1,16 +1,17 @@
 package com.example.tictactoe;
 
-import com.example.tictactoe.R;
+
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
+
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+//https://codinginflow.com/tutorials/android/tic-tac-toe/part-4-configuration-changes-reset-game
 
 public class SecondScreen extends AppCompatActivity implements View.OnClickListener{
     private Button[][] grid = new Button[3][3];
